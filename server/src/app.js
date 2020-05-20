a62868248aa4e39bb969e95862c47191cd5ba9ef
Nathan Englert <nathan.englert@newbalance.com>
2020-05-20 08:58:43 -0500

    - Fix CORS issue in development - Add call to API from client

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import OpenAPIBackend from 'openapi-backend';
@@ -36,6 +37,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 //app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => res.sendStatus(200));
